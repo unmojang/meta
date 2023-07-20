84ac59921e246e52706d547af9f784c93b8004dd
Evan Goode <mail@evangoo.de>
2023-07-19 22:44:19 -0400

    sigh, right, back to SSH urls

--- clone.sh
+++ clone.sh
@@ -4,6 +4,7 @@ set -e
 BASEDIR=$(dirname "$0")
 cd "${BASEDIR}" || exit 1
 BASEDIR=$(pwd)
+BASEDIR_ESCAPED="$(printf '%q' "$BASEDIR")"
 
 source config.sh
 if [ -f config/config_local.sh ]; then
@@ -13,9 +14,9 @@ fi
 set -x
 
 if [ ! -d "${UPSTREAM_DIR}" ]; then
-    git clone "${UPSTREAM_REPO}" "${UPSTREAM_DIR}"
+    GIT_SSH_COMMAND="ssh -i ${BASEDIR_ESCAPED}/config/upstream.key" git clone "${UPSTREAM_REPO}" "${UPSTREAM_DIR}"
 fi
 
 if [ ! -d "${LAUNCHER_DIR}" ]; then
-    git clone "${LAUNCHER_REPO}" "${LAUNCHER_DIR}"
+    GIT_SSH_COMMAND="ssh -i ${BASEDIR_ESCAPED}/config/launcher.key" git clone "${LAUNCHER_REPO}" "${LAUNCHER_DIR}"
 fi
